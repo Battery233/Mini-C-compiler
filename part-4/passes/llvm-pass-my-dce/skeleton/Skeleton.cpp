5316c05663d6384590f66b31beccc2cca6f68f0b
C Ye <31448923+Battery233@users.noreply.github.com>
2019-01-12 23:05:14 +0000

    Add file

--- part-4/passes/llvm-pass-my-dce/skeleton/Skeleton.cpp
+++ part-4/passes/llvm-pass-my-dce/skeleton/Skeleton.cpp
@@ -4,27 +4,39 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 using namespace llvm;
+using namespace std;
 
 namespace {
-  struct SkeletonPass : public FunctionPass {
+struct MyDCE : public FunctionPass {
     static char ID;
-    SkeletonPass() : FunctionPass(ID) {}
-
-    virtual bool runOnFunction(Function &F) {
-      errs() << "I saw a function called " << F.getName() << "!\n";
-      return false;
+    MyDCE()
+        : FunctionPass(ID)
+    {
     }
-  };
+
+    virtual bool runOnFunction(Function& F)
+    {
+        for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb) {
+            for (BasicBlock::iterator i = bb->begin(), e = bb->end(); i != e; ++i) {
+            }
+        }
+        errs() << "my DCE  for function: " << F.getName() << "!\n";
+        return false;
+    }
+};
 }
 
-char SkeletonPass::ID = 0;
+char MyDCE::ID = 0;
 
-// Automatically enable the pass.
-// http://adriansampson.net/blog/clangpass.html
-static void registerSkeletonPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
-  PM.add(new SkeletonPass());
+static void registerSkeletonPass(const PassManagerBuilder&,
+    legacy::PassManagerBase& PM)
+{
+    PM.add(new MyDCE());
 }
+
 static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerSkeletonPass);
+    RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
+        registerSkeletonPass);
+
+__attribute__((unused)) static RegisterPass<MyDCE>
+    X("live", "Iterative Liveness Analysis dead code elimination");
